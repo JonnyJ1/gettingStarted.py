f4a79f17ba7e576f5b3227cf00ce09d27f2a7367
JonnyJ1 <145157968+JonnyJ1@users.noreply.github.com>
2023-09-16 14:08:27 -0400

    Create gettingStarted.py

---
+++ gettingStarted.py
@@ -0,0 +1,50 @@
+### welcome_assignment_answers
+### Input - All nine questions given in the assignment.
+### Output - The right answer for the specific question.
+
+def welcome_assignment_answers(question):
+    if question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
+        answer = "pcap"
+    elif question == "Are encoding and encryption the same? - Yes/No":
+        answer = "No"
+    elif question == "Is it possible to decrypt a message without a key? - Yes/No":
+        answer = "No"
+    elif question == "Is it possible to decode a message without a key? - Yes/No":
+        answer = "Yes"
+    elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
+            answer = "No"
+    elif question == "What is the SHA256 hashing value of your NYU email and use the answer in your code - ": answer = "1e10d90c9436023872eca2b73c5972876a1dff9c6223d77a244270148cc8739c"
+    elif question == "Is MD5 a secured hashing algorithm? - Yes/No": answer = "No"
+    elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
+    answer = 4
+    elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
+        answer = 3
+return answer
+# Complete all the questions.
+
+if __name__ == "__main__":
+    #use this space to debug and verify that the program works
+    questions = ["In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
+"Are encoding and encryption the same? - Yes/No":
+"Is it possible to decrypt a message without a key? - Yes/No":
+"Is it possible to decode a message without a key? - Yes/No":
+"Is a hashed message supposed to be un-hashed? - Yes/No":
+"What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
+"Is MD5 a secured hashing algorithm? - Yes/No":
+"What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
+"What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":]
+
+   for debug_question in questions
+       print (debug_question)
+    print(welcome_assignment_answers(debug_question))
+
+#Questions:
+#"In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
+#"Are encoding and encryption the same? - Yes/No":
+#"Is it possible to decrypt a message without a key? - Yes/No":
+#"Is it possible to decode a message without a key? - Yes/No":
+#"Is a hashed message supposed to be un-hashed? - Yes/No":
+#"What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
+#"Is MD5 a secured hashing algorithm? - Yes/No":
+#"What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
+#"What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
